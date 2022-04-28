114769203f5d8fdd26ac0080a2513eb19f282feb
Richie Varghese <ric-v@outlook.com>
2022-04-29 00:06:08 +0530

    added her area with search bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import moment from "moment-timezone";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -8,24 +9,9 @@ import { useEffect, useState } from "react";
  * @returns {string}
  */
 const getCurrentTime = (): string => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  // get hours and pad with 0 if less than 10
-  const hours = date.getHours();
-  const hoursFormatted = hours < 10 ? `0${hours}` : hours;
-
-  // get minutes and pad with 0 if less than 10
-  const minutes = date.getMinutes();
-  const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
-
-  // get seconds and pad with 0 if less than 10
-  const seconds = date.getSeconds();
-  const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
-
-  return `${year}-${month}-${day} ${hoursFormatted}:${minutesFormatted}:${secondsFormatted} `;
+  return moment
+    .tz(new Date(), Intl.DateTimeFormat().resolvedOptions().timeZone)
+    .format("YYYY-MM-DD HH:mm:ss z/Z");
 };
 
 type Props = {
@@ -47,23 +33,26 @@ const Navbar = ({ children }: Props) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(getCurrentTime());
-    }, 1000);
+    }, 500);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className='h-full'>
+    <div className='bg-slate-200'>
       <div
-        className='h-96 shadow-[0px_50px_30px_-15px_rgba(0,0,0,0.33)] bg-gradient-to-br 
+        className='h-64 shadow-[0px_50px_30px_-15px_rgba(0,0,0,0.33)] bg-gradient-to-br 
           text-center from-cyan-900 to-slate-800 text-slate-300
           sm:shadow-[0px_50px_30px_-15px_rgba(0,0,0,0.4)]'>
-        <Link href={"/"}>
-          <a className='text-4xl font-nova-flat sm:text-6xl border-b-2 border-slate-400'>
-            And the time is ...
-          </a>
-        </Link>
-        <div className='text-5xl mt-14 font-nova-flat text-slate-300'>
-          {currentTime}
+        <div className='flex flex-col pt-5'>
+          <Link href={"/"}>
+            <a className='text-4xl font-nova-flat sm:text-6xl pb-5 border-b-2 border-slate-400 hover:text-slate-400'>
+              And the time is ...
+            </a>
+          </Link>
+        </div>
+        <div className='mt-14 font-nova-flat text-slate-300'>
+          <span className='text-teal-500 text-xl'>{"Local Time: "}</span>
+          <span className='text-2xl sm:text-5xl'>{currentTime}</span>
         </div>
       </div>
 
