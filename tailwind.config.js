96202a33918dd9ebe6106b388ce271807e050f96
Richie Varghese <ric-v@outlook.com>
2022-06-19 03:05:39 +0530

    folder restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   darkMode: "class",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       "nova-flat": '"Nova Flat"',
