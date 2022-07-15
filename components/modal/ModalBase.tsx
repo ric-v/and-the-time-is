8bf53cc5925414dcf2546de23f9afb0ac3af6282
Richie Varghese <ric-v@outlook.com>
2022-07-15 20:18:23 +0530

    modal theme enhancements

--- components/modal/ModalBase.tsx
+++ components/modal/ModalBase.tsx
@@ -14,14 +14,14 @@ const ModalBase = ({ body, actionBar }: Props) => {
 
       <div className="fixed z-10 inset-0 overflow-y-auto">
         <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
-          <div className="relative bg-slate-800 rounded-xl border border-dashed border-gray-600 text-left overflow-hidden  
+          <div className="relative rounded-xl border border-dashed border-gray-600 text-left overflow-hidden  
             shadow-[20px_40px_40px_5px_rgba(0,0,0,0.33)] transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-            <div className="bg-slate-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="bg-gradient-to-br from-gray-800 to-slate-900  px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="mt-3 text-center sm:mt-0 sm:ml-4">
                 {body}
               </div>
             </div>
-            <div className="bg-slate-700 px-4 py-3 sm:px-6 flex flex-row justify-end">
+            <div className="bg-gradient-to-br from-slate-700 to-slate-800 px-4 py-3 sm:px-6 flex flex-row justify-end">
               {actionBar}
             </div>
           </div>
