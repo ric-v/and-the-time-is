1e93c994402bc84bae2d1d054e39c16fc291b35e
Richie Varghese <ric-v@outlook.com>
2022-07-18 11:30:39 +0530

    modal button feedback reversed

--- components/modal/ModalButton.tsx
+++ components/modal/ModalButton.tsx
@@ -18,7 +18,7 @@ const ModalButton = ({ text, close, handleClick, classes, disabled }: Props) => 
                   focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 
                   sm:ml-3 sm:w-auto sm:text-sm transition duration-1000 ease-in-out
                   shadow-[10px_10px_20px_-5px_rgba(0,0,0,0.53)]
-                  bg-${close || disabled ? 'gray-800' : 'teal-700'} hover:bg-${close || disabled ? 'gray' : 'teal'}-900 ${classes}
+                  bg-${close ? 'gray-800' : 'teal-700'} hover:bg-${close ? 'gray' : 'teal'}-900 ${classes}
                   ${disabled ? 'cursor-not-allowed' : ''}`}
       onClick={handleClick}
     >
